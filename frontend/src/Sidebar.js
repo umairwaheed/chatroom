8f9ecb7456c4ea97367cc2645155f9850343a71f
Umair Khan <umair.waheed@gmail.com>
2021-12-12 22:11:55 +0500

    Improve chatroom list

--- frontend/src/Sidebar.js
+++ frontend/src/Sidebar.js
@@ -2,10 +2,8 @@ export default function Sidebar() {
   return (
     <div className="Sidebar">
       <h2>Chatrooms</h2>
-      <ul>
-        <li>Animals</li>
-        <li>Teams</li>
-      </ul>
+      <div className="menu-item">Animals</div>
+      <div className="menu-item">Teams</div>
     </div>
   );
 }
